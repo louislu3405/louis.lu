bdc6be48374f15b01c6f6a052fd0df615dfbd98a
louislu3405 <71736357+louislu3405@users.noreply.github.com>
2021-07-26 15:36:24 -0400

    clickable abour years

--- client/page_script.js
+++ client/page_script.js
@@ -1,8 +1,15 @@
 function switchYear(el) {
-    var years = document.getElementsByClassName('about-years');
-    for (var i = 0; i < years.length; i++) {
-        year = years[i];
+    var years_li = document.getElementsByClassName('about-years');
+    var years_p = document.getElementsByClassName('about-years-p');
+    for (var i = 0; i < years_li.length; i++) {
+        year = years_li[i];
         year.classList.remove('selected-years');
+        year.style.height = "auto";
+        years_p[i].style.display = "none"
+        if (years_li[i] == el) {
+            years_p[i].style.display = "block";
+        }
     }
     el.classList.add('selected-years');
+    el.style.height = "100px";
 }
