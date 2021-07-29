e7a92ca72ef7b5c7cbf4ef2d80df6df1e8e55260
louislu3405 <71736357+louislu3405@users.noreply.github.com>
2021-07-29 16:35:23 -0400

    connect years and map markers

--- client/page_script.js
+++ client/page_script.js
@@ -1,7 +1,6 @@
 function switchYear(el) {
     var years_li = document.getElementsByClassName('about-years');
     var years_p = document.getElementsByClassName('about-years-p'); 
-    console.log(years_p);
     var years_pBox = document.getElementsByClassName('about-years-desbox');
     for (var i = 0; i < years_li.length; i++) {
         year = years_li[i];
@@ -9,9 +8,7 @@ function switchYear(el) {
         year_pBox = years_pBox[i]
         // remove years bold font size and image bullet
         year.classList.remove('selected-years');
-        // year.style.height = "auto";
         // show/hide paragaph and reset height
-        console.log(years_p);
         year_p.classList.remove('about-years-p-selected');
         year_pBox.classList.remove('about-years-desbox-selected');
         if (years_li[i] == el) {
@@ -20,10 +17,13 @@ function switchYear(el) {
         }
     }
     el.classList.add('selected-years');
-    // el.style.height = "100px";
+    
+    // change markers on the map
+    var marker_id = el.id + "-marker"
+    document.getElementById(marker_id).click();
 }
 // About geojson data
-var about_geojson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[121.53762817382814,25.045170158679838]}},{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-113.67641687393188,48.79747892957555]}},{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-76.94013118743896,38.987634705293424]}}]}
+var about_geojson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"class": "about-marker-selected", "relateYear": "year1994"},"geometry":{"type":"Point","coordinates":[121.53762817382814,25.045170158679838]}},{"type":"Feature","properties":{"class": "about-marker", "relateYear": "year2017"},"geometry":{"type":"Point","coordinates":[-113.67641687393188,48.79747892957555]}},{"type":"Feature","properties":{"class": "about-marker", "relateYear": "year2019"},"geometry":{"type":"Point","coordinates":[-76.94013118743896,38.987634705293424]}}]};
 
 // mapbox map
 let map = new mapboxgl.Map({
@@ -42,7 +42,31 @@ about_geojson.features.forEach(function(marker) {
     
     // create a HTML element for each feature
     var el = document.createElement('div');
-    el.className = 'about-marker';
+    if (marker.properties.class == "about-marker"){
+        el.className = 'about-marker';
+    } else if (marker.properties.class == "about-marker-selected") {
+        el.className = 'about-marker-selected';
+    }
+
+    el.id = marker.properties.relateYear + "-marker";
+
+    // change map icon when clicking on the marker
+    el.addEventListener('click', function() {
+        // change marker
+        var selected = document.getElementsByClassName('about-marker-selected')[0];
+        selected.classList.replace('about-marker-selected', 'about-marker');
+
+        if (el.classList.contains('about-marker')) {
+            el.classList.replace('about-marker', 'about-marker-selected');
+        } else {
+            el.classList.replace('about-marker-selected', 'about-marker');
+        }
+
+        // change about year
+        var id = marker.properties.relateYear;
+        var year_el = document.getElementById(id);
+        switchYear(year_el);
+    })
 
     // make a marker for each feature and add to the map
     new mapboxgl.Marker(el)
